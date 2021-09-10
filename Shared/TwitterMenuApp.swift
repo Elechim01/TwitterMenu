9e117919ba1b79bba68d70df12ecdc7ec0694400
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-10 22:15:59 +0200

    Initial Commit

---
+++ Shared/TwitterMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterMenuApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 10/09/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
